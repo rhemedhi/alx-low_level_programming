204f2959deaf61efeb4cc6291b1039b0822b117a
username <michaelavornyo718@gmail.com>
2023-09-07 09:40:59 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = strlen(message);
+
+if (write(2, message, len) != len) 
+return 1;
+}
+return 1;
+}
