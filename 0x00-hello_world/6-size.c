a1a3feabf1d1aa780ef782b23a211baceb18735a
username <michaelavornyo718@gmail.com>
2023-09-07 08:20:06 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ long int a;
 long long int b;
 float c;
 
-printf("size of a char:%u byte(s)\n", sizeof(i));
-printf("size of an int:%u byte(s)\n", sizeof(j));
-printf("size of a long int:%u byte(s)\n", sizeof(a));
-printf("size of a long long int:%u byte(s)\n", sizeof(b));
-printf("size of a float:%u byte(s)\n", sizeof(c));
+printf("size of a char: %u byte(s)\n", sizeof(i));
+printf("size of an int: %u byte(s)\n", sizeof(j));
+printf("size of a long int: %u byte(s)\n", sizeof(a));
+printf("size of a long long int: %u byte(s)\n", sizeof(b));
+printf("size of a float: %u byte(s)\n", sizeof(c));
 return (0);
 }
