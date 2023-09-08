020f952135556a16c674845631e7ac81bce9e28a
username <michaelavornyo718@gmail.com>
2023-09-08 11:16:27 -0700

    loweranduppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints the lowercase alphabet from 'a' to 'z'
+ * and then uppercase alphabet from 'A' to 'Z' followed by
+ * a new line and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char l = 'a';
+char u = 'A';
+
+while(l <= 'z')
+{
+putchar(l);
+l++;
+}
+
+while (u <= 'Z')
+{
+putchar(u);
+u++;
+}
+putchar('\n');
+
+return (0);
+}
