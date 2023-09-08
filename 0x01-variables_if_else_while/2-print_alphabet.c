31af4a99be517d3e03e4f954de8148db18cbb45e
username <michaelavornyo718@gmail.com>
2023-09-08 10:49:14 -0700

    printalphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - alphabet in lowercase, followed by a new line.
+*
+* Always return(0)
+*/
+
+int main(void)
+{
+char c = 'a';
+
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar("\n");
+
+return (0);
+}
