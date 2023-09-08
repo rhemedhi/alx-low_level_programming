13186e1d5dd6aca7eccc398d0056bff64f8858be
username <michaelavornyo718@gmail.com>
2023-09-08 15:09:41 -0700

    printnumberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers of base 10 from 0
+ *
+ * Description: The function prints all base 10 numbers from 0
+ * Return: Always(0) Success
+ */
+
+int main(void)
+{
+
+int i = 0;
+
+for (i = 0; i < 10; i++)
+{
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+}
