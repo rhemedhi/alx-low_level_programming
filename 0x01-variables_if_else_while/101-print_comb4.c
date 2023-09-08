d4b15d4f513bd87e2b04eb8d01a9dc2868d4ef14
username <michaelavornyo718@gmail.com>
2023-09-08 16:37:00 -0700

    threedigits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - possible combos of 3 digits
+ * main - Generate all possible combinations of three different digits
  *
- * Return (0)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 int i, j, k;
 
 for (i = 48; i < 58; i++)
 {
-for (j = 48; j < 58; j++)
+for (j = i + 1; j < 58; j++)
 {
-for (k = 48; k < 58; k++)
+for (k = j + 1; k < 58; k++)
 {
-if (i == j || j == k || i == k)
-{
-continue;
-}
 putchar(i);
 putchar(j);
 putchar(k);
 
 if (i == 55 && j == 56 && k == 57)
 {
-break;
-
+putchar('\n');
 }
 else
 {
@@ -36,8 +30,7 @@ putchar(' ');
 }
 }
 }
-putchar('\n');
 }
-return (0);
 
+return (0);
 }
