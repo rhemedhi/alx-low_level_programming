cbf89fed91f9ca8a75372b0c2a732a58ff8c803b
username <michaelavornyo718@gmail.com>
2023-09-08 11:34:08 -0700

    printnumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints the lowercase alphabet from 'a' to 'z' followed by
+ * a new line and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int a = '0';
+
+for (a = 0; a < 10; a++)
+{
+printf("%d\n", a);
+}
+
+return (0);
+}
