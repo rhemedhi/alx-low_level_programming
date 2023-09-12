1b9bec2bb26b5cd5bdd024889af50e885382eb6d
username <michaelavornyo718@gmail.com>
2023-09-12 05:04:03 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 
-printf("_putchar");
+printf("_putchar\n");
 
 return (0);
 }
