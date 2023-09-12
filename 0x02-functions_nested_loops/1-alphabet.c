fea241a675632bd8b8f227fac260235876d2891a
username <michaelavornyo718@gmail.com>
2023-09-12 05:07:20 -0700

    functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_alphabet - Check description
@@ -8,9 +8,9 @@
 
 void print_alphabet(void)
 {
-char l;
+char i;
 
-for (l = 'a'; l <= 'z'; l++)
-_putchar(l);
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
 _putchar('\n');
 }
