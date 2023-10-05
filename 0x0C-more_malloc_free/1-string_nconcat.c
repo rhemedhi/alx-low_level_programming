8e29d2c31f5debf1a00fcb600884c342e2897676
Rhemedhi <michaelavornyo718@gmail.com>
2023-10-05 18:55:22 +0000

    moremallocfree

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,44 @@
 #include "main.h"
 
 /**
- * string_nconcat - concantinates two strings
- * @s1: destination
- * @s2: source
- * @n: number of bytes of st to concant
- *
- * Return: Null if functin faills
- * or ptr to new allocated string.
+*string_nconcat - Concatenates two strings using at
+*most an inputted number of bytes.
+*@s1: The first string.
+*@s2: The second string.
+*@n: The maximum number of bytes of s2 to concatenate to s1.
+*
+*Return: If the function fails - NULL.
+*Otherwise - a pointer to the concatenated space in memory.
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1 = 0;
-	unsigned int l2 = 0;
-	unsigned int counter1;
-	unsigned int counter2;
-	char *newpoint;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[l1] != '\0')
-	{
-		l1++;
-	}
-	while (s2[l2] != '\0')
-	{
-		l2++;
-	}
-	if (n >= l2)
-		n = l2;
-	newpoint = malloc(l1 + l2 + 1);
-	if (newpoint == NULL)
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
-	for (counter1 = 0; counter1 < l1; counter1++)
-	{
-		newpoint[counter1] = s1[counter1];
-	}
-	for (counter2 = 0; counter2 < n; counter2++)
-	{
-		newpoint[counter1 + counter2] = s2[counter2];
-	}
-	newpoint[counter1 + counter2] = '\0';
-	return (newpoint);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
