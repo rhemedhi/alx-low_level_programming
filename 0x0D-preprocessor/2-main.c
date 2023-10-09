1f53fd840235a2e3b7adf2711e418119a478eb73
Rhemedhi <michaelavornyo718@gmail.com>
2023-10-09 19:47:58 +0000

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
-nes (12 sloc)  261 Bytes
-
 #include <stdio.h>
+
 /**
  * main - Program compilation starts from the main
  * Description: this program prints the name of the file it was comipiled from
  * author: jonblesson
  * Return: 0 (success)
  */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef ABS_X
-#define ABS_X
-#define ABS(X) ((X < 0) ? (X * -1) : (X))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+
 #endif
