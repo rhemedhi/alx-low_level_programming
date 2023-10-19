db79816b1ea5adf10c058a5bf2887e81b58694cb
Rhemedhi <michaelavornyo718@gmail.com>
2023-10-19 23:15:27 +0000

    singlylinkedlists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list-len - return number of elements in a linked list_t list.
+ *
+ * @h: Pointer to linked list node.
+ *
+ * Return: number j elements in linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t j = 0;
+
+	while (h != NULL)
+	{
+		j++;
+		h = h->next;
+	}
+	return (j);
+}
