eef32d8c3f372a3278907b0504f5cf0948f24c24
Rhemedhi <michaelavornyo718@gmail.com>
2023-10-20 00:15:50 +0000

    singlylinkedlists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * torse - print item before function is executed
+ * Return: no return.
+ */
+
+void __attribute__((constructor)) torse()
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
