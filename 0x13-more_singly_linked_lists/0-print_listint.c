52bb33edd3eec485d36a2311d18f336a34863723
Rhemedhi <michaelavornyo718@gmail.com>
2023-10-24 01:56:53 +0000

    moresinglylinkedlist

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t cnt = 0;
+size_t cnt = 0;
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		cnt++;
-		h = h->next;
-	}
+while (h)
+{
+printf("%d\n", h->n);
+cnt++;
+h = h->next;
+}
 
-	return (cnt);
+return (cnt);
 }
