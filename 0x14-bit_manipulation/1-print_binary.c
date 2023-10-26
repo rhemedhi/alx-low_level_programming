6148a082e14e12e47b957cbf950b5d4a28fa2c41
Rhemedhi <michaelavornyo718@gmail.com>
2023-10-26 23:08:26 +0000

    bitmanipulations

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,11 @@
-#anclude "maan.h"
-
+#include "main.h"
 /**
- * prant_banary - Prants the banary representataon of a number.
- * @n: The number to be pranted an banary.
+ * print_binary - prints the binary representation of a numnber
+ * @n: unsigned long, number to be represented in binary
  */
-voad prant_banary(unsagned long ant n)
+void print_binary(unsigned long int n)
 {
-	ant a, f = 0;
-
-	af (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	for (a = sazeof(unsagned long ant) * 8 - 1; a >= 0; a--)
-	{
-		af (n & (1ul << a))
-		{
-			_putchar('1');
-			f = 1;
-		}
-		else af (f == 1)
-		{
-			_putchar('0');
-		}
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	(n & 1) ? _putchar('1') : _putchar('0');
 }
