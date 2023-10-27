80f45f61c262b8ce362aa017e8da26a2df72993f
Rhemedhi <michaelavornyo718@gmail.com>
2023-10-27 02:52:37 +0000

    bitmanipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,11 +8,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (n == NULL || index > 63)
-{
-return (-1);
-}
+	if (n == NULL || index > 63)
+	{
+		return (-1);
+	}
     
-*n = (*n | (1ul << index));
-return (1);
+	*n = (*n | (1ul << index));
+		return (1);
 }
